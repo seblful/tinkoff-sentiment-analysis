33bbf8bd364620a37b49eaaf0e2a57b012b0e1a0
seblful <alesha.chimba@gmail.com>
2023-12-12 23:03:08 +0300

    [EDIT] reformat with functions

--- scraping/scrape_api.py
+++ scraping/scrape_api.py
@@ -1,42 +1,68 @@
-from utils import format_date
+from utils import get_api_data, format_date, get_input_date, convert_data_to_json
 
+from datetime import datetime, date, time
+import pytz
 import requests
 import json
+import os
+
+import pandas as pd
 
 # Set variables
-current_cursor = 9999999999
-limit = 10
-url = f'https://www.tinkoff.ru/api/invest-gw/social/post/feed/v1/post/instrument/SBER?sessionId=QxZLiUIV31WxIZ4WonMwyIGI3UqG0zFO.ds-prod-api-101&appName=socialweb&appVersion=1.380.0&origin=web&platform=web&limit={limit}&cursor={current_cursor}&include=all'
+# STOCKS_NAME = ''
 
-# Get results and results content
-result = requests.get(url)
-content = result.content
+CURRENT_CURSOR = 9999999999
+RECORDINGS_LIMIT = 10
+URL_API = f'https://www.tinkoff.ru/api/invest-gw/social/post/feed/v1/post/instrument/SBER?sessionId=QxZLiUIV31WxIZ4WonMwyIGI3UqG0zFO.ds-prod-api-101&appName=socialweb&appVersion=1.380.0&origin=web&platform=web&limit={RECORDINGS_LIMIT}&cursor={CURRENT_CURSOR}&include=all'
 
-# Format bytes object to dict
-data = json.loads(content.decode('utf-8'))
+# DATE_START = get_input_date()
+# DATE_END = ''
+DATE_START = date(2023, 12, 12)
+DATETIME_START = datetime.combine(DATE_START, time.min)
+DATETIME_START = pytz.utc.localize(DATETIME_START)
 
-# Retrieve next cursor and items with data
-next_cursor = data['payload']['nextCursor']
+HOME = os.getcwd()
+CSV_PATH = os.path.join(HOME, 'pulse.csv')
 
-items = data['payload']['items']
 
-# Iterating through each recording
-for item in items:
+def process_data_json(data):
+    # Retrieve next cursor and items with data
+    next_cursor = data['payload']['nextCursor']
 
-    item_id = item['id']
+    items = data['payload']['items']
 
-    item_data = item['inserted']
+    # Iterating through each recording
+    for item in items:
 
-    item_reactions = item['reactions']
-    item_reactions_total_counts = item_reactions['totalCount']
-    item_reactions_counters = item_reactions['counters']
-    for counter in item_reactions_counters:  # add counter to each reaction
-        pass
+        item_id = item['id']
 
-    item_content = item['content']
-    text = item_content['text']  # format text with tags SBER
-    instruments = item_content['instruments']
-    for instrument in instruments:  # take tickers
-        pass
+        item_date = item['inserted']
+        item_datetime = format_date(item_date)
 
-print(len(items))
+        item_reactions = item['reactions']
+        item_reactions_total_counts = item_reactions['totalCount']
+        item_reactions_counters = item_reactions['counters']
+
+        for counter in item_reactions_counters:  # add counter to each reaction
+            item_reactions_type = counter['type']
+            item_reactions_count = counter['count']
+
+        item_content = item['content']
+        item_text = item_content['text']  # format text with tags SBER
+        item_instruments = item_content['instruments']
+        for instrument in item_instruments:  # take tickers
+            instrument_name = instrument['ticker']
+            instrument_price = instrument['price']
+
+        break
+
+    print(len(items))
+
+
+def main():
+    data = get_api_data(URL_API)
+    process_data_json(data)
+
+
+if __name__ == '__main__':
+    main()
