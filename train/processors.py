6eaaf5f68efac1a5a58b95aec9a22de60191638a
seblful <alesha.chimba@gmail.com>
2023-12-17 17:25:16 +0300

    [EDIT] add proxy and headers

--- train/processors.py
+++ train/processors.py
@@ -1,6 +1,8 @@
 import re
 import requests
 import json
+import random
+import time
 
 import pandas as pd
 from tqdm import tqdm
@@ -12,6 +14,9 @@ import demoji
 
 from autocorrect import Speller
 from deep_translator import GoogleTranslator
+from deep_translator.exceptions import RequestError
+
+from fake_headers import Headers
 
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch.nn.functional as F
@@ -20,8 +25,10 @@ import torch.nn.functional as F
 class UtilsClass:
     def __init__(self):
         self.__stop_words = None
-        self.proxy_json_url = 'https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/all/data.json'
+        # self.proxy_json_url = 'https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/all/data.json'
+        self.proxy_json_url = "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/http/data.json"
         self.__proxy_dict = None
+        self.__headers_list = None
 
         self.load_nlp()
 
@@ -39,10 +46,34 @@ class UtilsClass:
         if self.__proxy_dict is None:
             data = requests.get(self.proxy_json_url).content
             data_json = json.loads(data)
-            self.__proxy_dict = {item['protocol']
-                : item['proxy'] for item in data_json}
+            self.__proxy_dict = {}
+            for item in data_json:
+                key = item['protocol']
+                # self.__proxy_dict[key] = self.__proxy_dict.setdefault(
+                #     key, []).append(item['proxy'])
+                self.__proxy_dict[key] = self.__proxy_dict.get(
+                    key, []) + [item['proxy']]
 
         return self.__proxy_dict
+
+    @property
+    def headers_list(self):
+        if self.__headers_list is None:
+            self.__headers_list = [
+                Headers(headers=True).generate() for _ in range(50)]
+
+        return self.__headers_list
+
+    def get_random_proxies_dict(self):
+        proxy = {key: random.choice(value)
+                 for key, value in self.proxy_dict.items()}
+
+        return proxy
+
+    def get_random_headers_dict(self):
+        header = random.choice(self.headers_list)
+
+        return header
 
     def load_nlp(self):
         # !python -m spacy download ru_core_news_sm
@@ -56,7 +87,7 @@ class UtilsClass:
         text = re.sub("#\w+", " ", text)
         text = re.sub("https?://\S+|www\.\S+", " ", text)
         text = demoji.replace(text, ' ')
-        text = Speller(lang='ru')(text)
+        # text = Speller(lang='ru')(text)
         text = re.sub(r"[^a-zA-ZА-Яа-я]", " ", text)
 
         text = " ".join(word for word in text.split()
@@ -68,12 +99,21 @@ class UtilsClass:
         return text
 
     def translate_text(self, text):
+        proxy = self.get_random_proxies_dict()
+        header = self.get_random_headers_dict()
+
+        print("Random proxy: ", proxy)
+        print("Random header: ", header)
+
         translated = GoogleTranslator(
-            source='ru', target='en', proxies=self.proxy_dict).translate(text)
+            source='ru', target='en', proxies=proxy, headers=header).translate(text)
 
         return translated
 
     def classify_text(self, text, model, tokenizer):
+        # Decreate length of text to model maximum
+        text = text[:500]
+
         encoded_input = tokenizer(text, return_tensors='pt')
 
         # Get the logits
@@ -105,6 +145,7 @@ class TrainDataCreator:
                  ):
         self.df = pd.read_csv(csv_filename)
         self.train_df = self.df.sample(sample_size)
+        self.sample_size = sample_size
 
         self.util = UtilsClass()
 
@@ -112,13 +153,33 @@ class TrainDataCreator:
             model_name=model_name)
         self.model_id2label = self.model.config.id2label
 
+    def translate_text_df(self, name_of_column):
+        self.train_df['eng_text'] = ''
+        for index, value in tqdm(self.train_df[name_of_column].items(), desc='Translating text', total=self.sample_size):
+            i = index
+            while i == index:
+                try:
+                    self.train_df.at[index,
+                                     'eng_text'] = self.util.translate_text(value)
+                    i = -1
+                except RequestError as request_error:
+                    time_to_sleep = random.uniform(10, 20)
+                    time.sleep(time_to_sleep)
+                    print(
+                        f"Too many requests for proxy, trying again after sleeping for {time_to_sleep:.2f} seconds.")
+
+                    continue
+
     def process(self):
         tqdm.pandas(desc='Cleaning text')
         self.train_df['clean_text'] = self.train_df.loc[:,
                                                         'text'].progress_apply(self.util.clean_text)
-        tqdm.pandas(desc='Translating text')
-        self.train_df['eng_text'] = self.train_df.loc[:,
-                                                      'clean_text'].progress_apply(self.util.translate_text)
+
+        # tqdm.pandas(desc='Translating text')
+        # self.train_df['eng_text'] = self.train_df.loc[:,
+        #                                               'clean_text'].progress_apply(self.util.translate_text)
+        self.translate_text_df(name_of_column='clean_text')
+
         tqdm.pandas(desc='Classifying text')
         self.train_df[['predicted_label', 'predicted_score']] = self.train_df.loc[:,
                                                                                   'eng_text'].progress_apply(self.util.classify_text, args=[self.model, self.tokenizer])
